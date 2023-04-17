aa79458dbdfce8b37f83266a7b6e2dcc0fe1d3b8
CC <zzzzzzzz233mm@163.com>
2023-04-17 12:36:52 +0800

    添加了数字框控件

---
+++ Views/Control/NumericBox.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CustomHotKey.Views.MyControl
+{
+    /// <summary>
+    /// 按照步骤 1a 或 1b 操作，然后执行步骤 2 以在 XAML 文件中使用此自定义控件。
+    ///
+    /// 步骤 1a) 在当前项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根
+    /// 元素中:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:CustomHotKey.Views.Control"
+    ///
+    ///
+    /// 步骤 1b) 在其他项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根
+    /// 元素中:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:CustomHotKey.Views.Control;assembly=CustomHotKey.Views.Control"
+    ///
+    /// 您还需要添加一个从 XAML 文件所在的项目到此项目的项目引用，
+    /// 并重新生成以避免编译错误:
+    ///
+    ///     在解决方案资源管理器中右击目标项目，然后依次单击
+    ///     “添加引用”->“项目”->[浏览查找并选择此项目]
+    ///
+    ///
+    /// 步骤 2)
+    /// 继续操作并在 XAML 文件中使用控件。
+    ///
+    ///     <MyNamespace:NumericBox/>
+    ///
+    /// </summary>
+    public class NumericBox : TextBox
+    {
+
+        public int Step
+        {
+            get { return (int)GetValue(StepProperty); }
+            set { SetValue(StepProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Step.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StepProperty =
+            DependencyProperty.Register("Step", typeof(int), typeof(NumericBox), new PropertyMetadata(0));
+
+        public int Max
+        {
+            get { return (int)GetValue(MaxProperty); }
+            set { SetValue(MaxProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Max.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MaxProperty =
+            DependencyProperty.Register("Max", typeof(int), typeof(NumericBox), new PropertyMetadata(0));
+
+        public int Min
+        {
+            get { return (int)GetValue(MinProperty); }
+            set { SetValue(MinProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Min.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MinProperty =
+            DependencyProperty.Register("Min", typeof(int), typeof(NumericBox), new PropertyMetadata(0));
+
+        static NumericBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NumericBox),
+            new FrameworkPropertyMetadata(typeof(NumericBox)));
+
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            
+            var up = GetTemplateChild("up") as Button;
+            var down = GetTemplateChild("down") as Button;
+            up.Click += (s, e) =>
+            {
+                if (int.Parse(Text) >= Min && int.Parse(Text) <= Max)
+                {
+                    Text = (int.Parse(Text) + Step).ToString();
+                }
+            };
+            down.Click += (s, e) =>
+            {
+                if (int.Parse(Text) >= Min && int.Parse(Text) <= Max)
+                {
+                    Text = (int.Parse(Text) + Step).ToString();
+                }
+            };
+        }
+    }
+}
